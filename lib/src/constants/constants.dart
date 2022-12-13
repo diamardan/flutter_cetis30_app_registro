7b51b14d7b5751b83865bda65592322364bd3d11
Daniel Díaz Martínez <diamardan@gmail.com>
2022-12-12 20:57:43 -0600

    commit para las localnotifications

--- lib/src/constants/constants.dart
+++ lib/src/constants/constants.dart
@@ -1,7 +1,7 @@
 class AppConstants {
   //backendUrl hace referencia ya sea a la url de producción o a la de desarrollo
   static const backendUrl = 'https://escuelas.identaflix.com/api/v1/firebase';
-  //static const backendUrl = 'http://192.168.1.72:5000/api/v1/firebase';
+  //static const backendUrl = 'http://192.168.1.64:5000/api/v1/firebase';
   static const backendPublicUrl = 'https://escuelas.identaflix.com/public';
   static const backendBaseUrl = 'https://escuelas.identaflix.com/api/v1';
   //static const whatsappNumber = '+5215520779800';
